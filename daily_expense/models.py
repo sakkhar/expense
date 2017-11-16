4bc5280cd9aa31187dace8c0be1bc684f2af0d4c
sahasakkhar <sakkhar@mail.com>
2017-11-17 00:28:04 +0600

    Django Daily Expense Management System

--- daily_expense/models.py
+++ daily_expense/models.py
@@ -1,3 +1,16 @@
+from audioop import reverse
+
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Expense(models.Model):
+    id = models.AutoField(primary_key=True)
+    expense_date = models.DateTimeField()
+    expense_details = models.CharField(null=True, blank=True, max_length=200, help_text="Enter expense details")
+    expense_amount  = models.FloatField(null=True, help_text="Enter expense amount")
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse('expense details', args=[str(self.id)])
+
